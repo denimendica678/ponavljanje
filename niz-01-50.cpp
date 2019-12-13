55952c5e6ad0907df2c2ec8b5c270b864f7c32ef
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2019-12-13 13:03:01 +0100

    Add files via upload

---
+++ niz-01-50.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int i;
+	
+	for(i=0; i<=4; i++){
+		for(int y=0; y<=9; y++){
+			cout<<i<<"-"<<y<<endl;
+			
+		}
+	}
+	cout<<"5-0"<<endl;
+	system("pause");
+	return 0;
+}
+
---
+++ niz-1-20.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int i;
+	for(i=1; i<=20; i++){
+		
+		cout<<i;
+	    
+	if(i==20){
+		cout<<".";
+		
+	}else{
+		cout<<",";
+	}
+	}
+	
+	
+	system ("pause");
+	return 0;
+}
