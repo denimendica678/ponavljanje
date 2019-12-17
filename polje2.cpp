c77caeec2ab7033ebe7e9bf37609724ed943c219
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2019-12-17 14:04:49 +0100

    Add files via upload

---
+++ polje2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main () {
+	
+	int n;
+	
+	cout<<"gonjajjj se deèkoooo."<<endl;
+	cin>>n;
+	
+	int p[n][n]={{n}};
+	
+
+	
+	system ("pause");
+	return 0;
+}
